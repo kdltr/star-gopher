73afee98d823c47fdbc23ce401ef97b9c46f2edd
Kooda <kooda@upyum.com>
2019-07-14 20:19:35 +0200

    Affichage des horaires en durée relative

--- star-gopher.scm
+++ star-gopher.scm
@@ -1,4 +1,16 @@
-(import (chicken format) (chicken process-context) (chicken string) (chicken time posix) (srfi 13) http-client openssl uri-common medea phricken)
+(import
+  (chicken format)
+  (chicken process-context)
+  (chicken string)
+  (chicken time posix)
+  (srfi 1)
+  (srfi 13)
+  (srfi 71)
+  http-client
+  openssl
+  uri-common
+  medea
+  phricken)
 
 (client-software (cons '("Star Gopher" "0.1" #f) (client-software)))
 
@@ -25,6 +37,7 @@
                        (facet . sens)
                        (facet . destination)
                        (facet . precision)
+                       (facet . nomarret)
                        (apikey . ,*api-key*)
                        (timezone . ,(current-timezone))
                        (refine.idligne . ,line-id)
@@ -32,25 +45,73 @@
                        (refine.idarret . ,stop-id))))
 
 
+;; Date and duration madness
+
+;; Help, we need a good time & duration API
+
+;; API Dates are represented as strings of this format: yyyy-mm-ddTHH:MM:SS+ZZ:ZZ"
+
+(define (api-date->time str)
+  (string->time str "%Y-%m-%dT%H:%M:%S+"))
+
+(define (time- d1 d2)
+  (- (local-time->seconds d1)
+     (local-time->seconds d2)))
+
+;; Duration in seconds -> duration in hours, minutes, seconds
+(define (seconds->duration secs)
+  (let* ((big-minutes seconds (quotient&remainder secs 60))
+         (hours minutes (quotient&remainder big-minutes 60)))
+    (list hours minutes)))
+
+(define (duration->string dur)
+  (if (zero? (car dur))
+      (if (zero? (cadr dur))
+          "<1 min"
+          (sprintf "~A min" (cadr dur)))
+      (sprintf "~A hr ~A min" (car dur) (cadr dur))))
+
+
 ;; Actual work
 
 (define (fetch uri)
   (with-input-from-request uri #f read-json))
 
+(define (realtime-traffic-for line-id direction stop-id)
+  (let* ((uri (realtime-traffic-uri line-id direction stop-id))
+         (json (fetch uri))
+         (records (vector->list (alist-ref 'records json)))
+         (first-fields (alist-ref 'fields (car records))))
+    (cons*
+      (sprintf "Ligne ~A direction ~A"
+               (alist-ref 'nomcourtligne first-fields)
+               (alist-ref 'destination first-fields))
+      (sprintf "Arrêt: ~A"
+               (alist-ref 'nomarret first-fields))
+      (sprintf "Données datant du ~A"
+               (time->string (api-date->time
+                               (alist-ref 'record_timestamp (car records)))
+                             "%d/%m/%Y %H:%M:%S"))
+      ""
+      (format-realtime-traffic-data records))))
+
 (define (format-realtime-traffic-data records)
-  ;; TODO relative time
-  (define (cleanup-date str)
-    (cadr (string-split str "T+")))
-  (define (fmt-record rec)
-    (sprintf "~a -> ~a: ~a (~a)"
-             (alist-ref 'nomcourtligne rec)
-             (alist-ref 'destination rec)
-             (if (string=? (alist-ref 'precision rec)
+  (define (fmt-record record-fields record-timestamp)
+    (let* ((departure-time (api-date->time (alist-ref 'depart record-fields)))
+           (record-time (api-date->time record-timestamp))
+           (diff (time- departure-time record-time))
+           (duration (seconds->duration diff)))
+    (sprintf "~a ~a (~a)"
+             (if (string=? (alist-ref 'precision record-fields)
                            "Temps réel")
-                 (cleanup-date (alist-ref 'depart rec))
+                 #\space
                  #\*)
-             (cleanup-date (alist-ref 'departtheorique rec))))
-  (map (lambda (r) (fmt-record (alist-ref 'fields r))) records))
+             (duration->string duration)
+             (time->string (api-date->time (alist-ref 'departtheorique record-fields))
+                           "%H:%M:%S"))))
+  (map (lambda (r) (fmt-record (alist-ref 'fields r)
+                               (alist-ref 'record_timestamp r)))
+    records))
 
 
 ;; Gopher frontend
@@ -69,16 +130,13 @@
 (define (realtime-traffic-handler req)
   (define (sanitize-input stop-id line-id direction)
     (list (string-pad line-id 4 #\0) direction stop-id))
-  (let* ((args (apply sanitize-input (request-matches req)))
-         (uri (apply realtime-traffic-uri args))
-         (json (fetch uri))
-         (records (vector->list (alist-ref 'records json))))
-    (for-each
-      send-line
-      (format-realtime-traffic-data records))
+  (let ((args (apply sanitize-input (request-matches req))))
+    (for-each send-line (apply realtime-traffic-for args))
+    (send-lastline)
     #t))
 
 (handlers (list (match-selector "" root-handler)
+                (match-selector "/" root-handler)
                 (match-selector
                   '(: "/" ($ (+ num)) "/" ($ (+ num)) "/" ($ (+ num)))
                   realtime-traffic-handler)))
